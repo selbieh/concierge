dbe511b2c94d936e13e0703a6832f952125444d5
elbieh <sayed.elbieh@yourparts.com>
2022-08-24 19:52:33 +0200

    fix fcm bulk notifications

--- customers/models/invitation_code.py
+++ customers/models/invitation_code.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class InvitationCode(models.Model):
-    code = models.CharField(max_length=150, null=False, blank=False)
+    code = models.CharField(max_length=150, null=False, blank=False,unique=True)
     used = models.BooleanField(default=False)
     customer = models.ForeignKey("customers.Customer", on_delete=models.CASCADE, related_name="customer_invitation_codes")
 
