4030e80c6e7b6c356b6ee6e4dd38093f42fdab25
tamereltonsy <t.eltonsy@gmail.com>
2023-04-30 18:38:26 +0200

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import Group, Permission
 from phonenumber_field.formfields import PhoneNumberField
 from .models import User
-from django import forms
 
 class UserCreationModelForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -12,19 +11,38 @@ class UserCreationModelForm(UserCreationForm):
     full_name = forms.CharField(max_length=55, required=True)
     dob = forms.DateField(required=False, widget=forms.TextInput(attrs={'type': 'date'}))
     avatar = forms.FileField(required=False)
+    is_active = forms.BooleanField(required=False)
+    invitation_code = forms.ModelChoiceField(queryset=InvitationCode.objects.all(), required=False)
+    customer = forms.ModelChoiceField(queryset=Customer.objects.all(), required=False)
+    groups = forms.ModelMultipleChoiceField(queryset=Group.objects.all(), required=False)
+    user_permissions = forms.ModelMultipleChoiceField(queryset=Permission.objects.all(), required=False)
 
     class Meta:
         model = User
-        fields ='__all__'
+        fields = ('email', 'mobile', 'full_name', 'dob', 'avatar', 'password1', 'password2', 'is_active', 'invitation_code', 'customer', 'groups', 'user_permissions')
 
     def save(self, commit=True):
         user = super(UserCreationModelForm, self).save(commit=False)
         user.password = make_password(self.cleaned_data['password1'])
         if commit:
             user.save()
+            # Add user to groups
+            group_ids = self.cleaned_data.get('groups', [])
+            for group_id in group_ids:
+                group = Group.objects.get(pk=group_id)
+                user.groups.add(group)
+            # Add user permissions
+            permission_ids = self.cleaned_data.get('user_permissions', [])
+            permissions = Permission.objects.filter(pk__in=permission_ids)
+            user.user_permissions.set(permissions)
         return user
-
 class CustomUserAdmin(UserAdmin):
     add_form = UserCreationModelForm
-    
-admin.site.register(User, CustomUserAdmin)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'full_name', 'mobile', 'dob', 'avatar', 'is_active', 'invitation_code', 'customer', 'groups', 'user_permissions')}
+        ),
+    )
+
+    # other admin options...
