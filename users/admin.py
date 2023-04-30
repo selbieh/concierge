93cedce5999800af61de7257ea6d228ccb077f2c
tamereltonsy <t.eltonsy@gmail.com>
2023-04-30 18:05:32 +0200

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,25 @@
-from django.contrib import admin
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from phonenumber_field.formfields import PhoneNumberField
 
-# Register your models here.
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    mobile = PhoneNumberField(required=True)
+    full_name = forms.CharField(max_length=55, required=True)
+    dob = forms.DateField(required=False, widget=forms.TextInput(attrs={'type': 'date'}))
+    avatar = forms.FileField(required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'mobile', 'full_name', 'dob', 'avatar', 'password1', 'password2')
+        
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    search_fields = ['full_name','mobile']
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+
+admin.site.register(User, CustomUserAdmin)
+
