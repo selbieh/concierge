000077f27148ef1f2ef6494e5c20a36fa2fa11d8
tamereltonsy <t.eltonsy@gmail.com>
2023-04-30 18:23:25 +0200

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,11 @@
-from django import forms
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 from phonenumber_field.formfields import PhoneNumberField
+from .models import User
 
-class CustomUserCreationForm(UserCreationForm):
+class UserCreationModelForm(UserCreationForm):
     email = forms.EmailField(required=True)
     mobile = PhoneNumberField(required=True)
     full_name = forms.CharField(max_length=55, required=True)
@@ -12,14 +14,22 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ( 'email', 'mobile', 'full_name', 'dob', 'avatar', 'password1', 'password2')
-        
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+        fields = ('email', 'mobile', 'full_name', 'dob', 'avatar', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(UserCreationModelForm, self).save(commit=False)
+        user.password = make_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
+    add_form = UserCreationModelForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'full_name', 'mobile', 'dob', 'avatar')}
+        ),
+    )
 
 admin.site.register(User, CustomUserAdmin)
-
